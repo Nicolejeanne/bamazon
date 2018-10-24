5d5b2780c77684e519d4975a8e79d466f4e97bdf
NicJM <nicolejeanne@users.noreply.github.com>
2018-10-24 18:53:57 -0500

    js additions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,10 +17,56 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    // run the displayAllItems function after the connection is made to show the user all the products
-    displayAllItems();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the displayAllItems function after the connection is made to show the user all the products
+  start();
+});
 
-function displayAllItems()
+// function which prompts the user to display items for sale
+function start() {
+  inquirer
+    .prompt({
+      name: "showItems",
+      type: "confirm",
+      message: "Would you like to see all items available for sale?",
+      default: true
+    })
+    .then(function (answer) {
+      // based on their answer, either show the items or show goodbye message
+      if (answer.showItems === true) {
+        displayAllItems();
+      } else {
+        console.log("Sorry you don't feel like shopping! Try again later!")
+      }
+    });
+}
+
+function displayAllItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+    howMany();
+  });
+  
+}
+/*
+function howMany() {
+  inquirer
+    .prompt({
+      name: "qtyToBuy",
+      type: "input",
+      message: "How many of the item you would like to purchase?",
+    })
+    .then(function (answer) {
+      // based on their answer, either show the items or show goodbye message
+      if (answer is not available) {
+        console.log("Insufficient quantity in stock!");
+      } else {
+        // update database
+        // show the customer the total of their purchase
+        console.log("Thanks for your purchase!")
+        start();
+      }
+    });*/
